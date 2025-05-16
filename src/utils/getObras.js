6ae915ef7c0a87b6da9aa648210a3000aacce097
Herson Geovanni Lopez Campos <usis031120@ugb.edu.sv>
2025-05-15 22:40:04 -0600

    cambios

--- src/utils/getObras.js
+++ src/utils/getObras.js
@@ -1,6 +1,14 @@
 
 import { imagenDesdeLaNiebla, imagenElLagoDeLosInerfectos, imagenElMonstruoDelPijamaAzul, imagenElNacimientoDelRey, imagenEscritorDeEncantamientosNormal, imagenLaMuerteMeEnamora, imagenSiniestroAnochecer } from "./getImages";
 
+import libroDesdeLaNiebla from "../assets/files/DESDE LA NIEBLA DESCARGA.pdf"
+import libroDragonDelRey from "../assets/files/EL DRAGON DEL REY PARTE 1  DESCARGA.pdf"
+import libroDragonDelRey2 from "../assets/files/EL DRAGON DEL REY PARTE 2  DESCARGA.pdf"
+import libroEscritorDeEncantamientos from "../assets/files/EL ESCRITOR DE ENCANTAMIENTOS DESCARGA.pdf"
+import libroMonstruo from "../assets/files/EL MONSTRUO DEL PIJAMA AZUL DESCARGA.pdf"
+import libroLaMuerteMeEnamora from "../assets/files/LA MUERTE ME ENAMORA DESCARGA.pdf"
+import libroSiniestroAnochecer from "../assets/files/SINIESTRO ANOCHECER DESCARGA.pdf"
+
 export const obras = [
 
 	{
@@ -8,6 +16,7 @@ export const obras = [
 		tipo: "Cuentos para niños",
 		fechaLanzamiento: "8 de julio de 2020",
 		anio: "2020",
+		descargar: libroMonstruo,
 		num: "172",
 		imagen: imagenElMonstruoDelPijamaAzul,
 		desc: "Esta es la historia de un niño llamado Alex, que vivía inmerso en un estado casi de abandono o más bien, de descuido por sus padres. El niño, al sentirse solo, decide vivir en un mundo de fantasías, para sobrellevar esa situación, así como también, en el transcurso de la historia, sucede algo trágico en su familia, perdiendo lo que más amaba.Alex, decide crear una historia para cada día del mes y de esta manera, jugar e imaginar una vasta cantidad de aventuras y personajes reales e irreales, para sentir que es alguien importante en este mundo.",
@@ -20,6 +29,7 @@ export const obras = [
 		tipo: "Novela romántica",
 		fechaLanzamiento: "19 de marzo de 2021",
 		anio: "2021",
+		descargar: libroLaMuerteMeEnamora,
 		num: "333",
 		imagen: imagenLaMuerteMeEnamora,
 		desc: "Con esta novela romántica, te incitarás a preguntarte: ¿Te has atrevido alguna vez a desafiar a la muerte cara a cara? Prepárate para sumergirte en la fascinante historia de Dayana, una estudiante de enfermería rebosante de juventud, amor por la vida y una pasión desbordante.",
@@ -32,6 +42,7 @@ export const obras = [
 		tipo: "Novela de ficción",
 		fechaLanzamiento: "27 de septiembre de 2021",
 		anio: "2021",
+		descargar: libroDesdeLaNiebla,
 		num: "155",
 		imagen: imagenDesdeLaNiebla,
 		desc: "Esta historia nos presenta la trágica vida de un hombre, quien vivía muy feliz junto a su esposa y su pequeño hijo Dylan, el cual, al despreciar una propuesta realizada por una malvada hechicera; es condenado a vivir eternamente con una forma monstruosa. Con el pasar de los siglos, este monstruo tiene la oportunidad de conocer a un pequeño niño de cinco años de edad, con quien llega a crear un gran vínculo de amistad, a pesar de su condición y apariencia. Este acercamiento de amigos, les conlleva pasar por algunas dificultades, ya que el niño era visitado en secreto por el monstruo en múltiples ocasiones, dejando al final un suceso trágico para ambos; luego de vivir una hermosa y mágica aventura.",
@@ -44,6 +55,7 @@ export const obras = [
 		tipo: "1a parte trilogía",
 		fechaLanzamiento: "10 de agosto de 2023",
 		anio: "2023",
+		descargar: libroDragonDelRey,
 		num: "150",
 		imagen: imagenElNacimientoDelRey,
 		desc: "Una época antigua, donde hechiceros poblaban vastos territorios; y se vivía entre una aparente calma, que duraba de solsticio a solsticio. La ambición de dominio por parte de fuerzas siniestras, conllevan el deseo de obtener un poder que será capaz de someter a voluntad a un majestuoso dragón. Una pronta y sangrienta batalla tendrá lugar, luego de 600 años de la desaparición del último dragón existente, desatándose por un par de décadas, entre soldados reales y guerreros demoníacos, en un ambiente lleno de terror, traición, catástrofes, profecías, destinos trazados, hechizos, asesinatos y maldiciones. La salvación está fijada en un recién nacido, quien será protegido por una poderosa hechicera, dispuesta, hasta dar su vida por él.",
@@ -56,6 +68,7 @@ export const obras = [
 		tipo: "2a parte trilogía",
 		fechaLanzamiento: "7 de mayo de 2023",
 		anio: "2023",
+		descargar: libroDragonDelRey2,
 		num: "151",
 		imagen: imagenElLagoDeLosInerfectos,
 		desc: "En las antiguas y mágicas tierras de Zyelanor, de las Regiones Zelvétrion, Kellan descubre su poder para dominar dragones y junto a valientes aliados se enfrentan a guerreros oscuros. Unidos, emprenden una peligrosa aventura para detener al despiadado brujo Adabec. En su viaje, seres enigmáticos, condenados a un letargo eterno, son la clave para despertar al dragón. ¿Podrá Kellan salvar Zyelanor de las tinieblas? Descúbrelo en esta épica historia de hechiceros, dragones y misterios ancestrales.",
@@ -68,6 +81,7 @@ export const obras = [
 		tipo: "Terror",
 		fechaLanzamiento: "24 de julio de 2024",
 		anio: "2024",
+		descargar: libroSiniestroAnochecer,
 		num: "116",
 		imagen: imagenSiniestroAnochecer,
 		desc: "En las sombras del Lago Luzdanggel, una maldición ancestral condena a espíritus a un tormento eterno. Despojados de sus cuerpos, susurran lamentos y atraen almas con un poder siniestro. La leyenda arraiga miedo en los corazones de quienes la conocen. Los habitantes de Osmahyia evitan sus aguas, temerosos de ecos y sombras etéreas. En noches sin luna, los seres cazan almas inocentes, dejando relatos de desapariciones y susurros fantasmales. La trama explora la desesperación de quienes se cruzan con los condenados. Los protagonistas luchan por liberar al lago de su maldición, enfrentándose a una oscuridad inefable antes de que sus propias almas sean consumidas por el lamento eterno, que acecha cada “Siniestro Anochecer”.",
@@ -80,6 +94,7 @@ export const obras = [
 		tipo: "Novela romántica",
 		fechaLanzamiento: "20 de enero de 2025",
 		anio: "2025",
+		descargar: "",
 		num: "219",
 		imagen: imagenEscritorDeEncantamientosNormal,
 		desc: "En la tranquila biblioteca de su pueblo, una lectora apasionada descubre un libro que parece escrito para ella: un romance entre un mago y un hada que desafía el tiempo y el espacio. Pero lo que comienza como una historia fascinante pronto se transforma en algo mucho más personal. Entre sus páginas, secretos enigmáticos se entrelazan con su propia vida. Mientras desvela pistas ocultas, se adentra en una aventura donde magia y realidad se funden, enfrentando los misterios del amor, el destino y los secretos que podrían cambiarlo todo. Romántica, misteriosa y llena de magia, 'El Escritor de Encantamientos' es una historia para quienes creen que el amor verdadero puede encontrarse en los lugares más insospechados, y que seguir las huellas del corazón siempre lleva a un destino inesperado.",
